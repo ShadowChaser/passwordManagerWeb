bc40a8fb592996eac4898d0ace9491685e6a5376
Abhishek <abhishekkundu86@gmail.comm>
2023-03-26 14:41:11 +0530

    Nav bar added

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,13 +1,42 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 import profilepic from '../profilepic.jpg'
 
 export default function Profile() {
   return (
-    <div class="d-flex flex-row-reverse bd-highlight">
-        <div class="p-2 bd-highlight" style={Styles.profileImgContainer}>
-            <img src={profilepic} alt="Profile" style={Styles.image}/>
-        </div>
-    </div>
+    <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+    <Container>
+      <Navbar.Brand href="#home">Password Manager</Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="me-auto">
+          <Nav.Link href="#features">Features</Nav.Link>
+          <Nav.Link href="#pricing">Pricing</Nav.Link>
+          <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">
+              Another action
+            </NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="#action/3.4">
+              Separated link
+            </NavDropdown.Item>
+          </NavDropdown>
+        </Nav>
+        <Nav>
+          <Nav.Link eventKey={2} href="#memes">
+          <div style={Styles.profileImgContainer}>
+            <img src={profilepic} style={Styles.image}/>
+            </div>
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Container>
+  </Navbar>
   )
 }
 
@@ -20,8 +49,8 @@ const Styles=({
         height: 35,
         width: 35,
         borderRadius: "50%",
-        marginTop:"-5px",
-        marginLeft:"-5px"
+        marginTop:"3px",
+        marginLeft:"3px"
     },
     profileImgContainer: {
         height: 45,
@@ -29,8 +58,6 @@ const Styles=({
         borderRadius: "50%",
         borderStyle: "solid",
         borderColor: 'blue', 
-        borderWidth:2,
-        marginRight:"1%",
-        marginTop:"5px"
+        borderWidth:2
       },
 })
