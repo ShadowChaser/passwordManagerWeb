dfa86e406da1834513a6e24f490fafe916ca3c2b
Abhishek <abhishekkundu86@gmail.comm>
2023-04-01 23:19:35 +0530

    path changes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import HomePage from "./page/HomePage";
 import Login from "./components/login";
 import "bootstrap/dist/css/bootstrap.min.css";
-import {BrowserRouter as Router,Routes,Route,Navigate} from "react-router-dom";
+import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
 import { ToastContainer } from 'react-bootstrap';
 import {UserContext} from './context/UserContext';
 import PagenotFound from "./components/Pagenotfound";
@@ -14,8 +14,8 @@ function App() {
       <ToastContainer/>
         <UserContext.Provider value={{user,setUser}}>
           <Routes>
-              <Route exact path="/" element={<Login/>} />
-              <Route exact path="/home" element={<HomePage/>} />
+              <Route exact path="/login" element={<Login/>} />
+              <Route exact path="/" element={<HomePage/>} />
               <Route exact path="*" element={<PagenotFound/>}/>
           </Routes>
         </UserContext.Provider> 
